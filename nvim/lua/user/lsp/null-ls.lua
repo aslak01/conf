e2bbcf03c9733c68886bbbb9455c8adf2dacc775
aslak <aslak.bakkeland@everyday.no>
2022-07-11 11:57:47 +0200

    fix lsp

--- nvim/lua/user/lsp/null-ls.lua
+++ nvim/lua/user/lsp/null-ls.lua
@@ -1,52 +1,38 @@
-local null_ls = require("null-ls")
-local b = null_ls.builtins
-
-local diagnostics_code_template = "#{m} [#{c}]"
-
-local with_root_file = function(...)
-    local files = { ... }
-    return function(utils)
-        return utils.root_has_file(files)
-    end
+local null_ls_status_ok, null_ls = pcall(require, "null-ls")
+if not null_ls_status_ok then
+  return
 end
 
-local sources = {
-    -- formatting
-    b.formatting.prettier,
-    b.formatting.fish_indent,
-    b.formatting.shfmt,
-    b.formatting.clang_format,
-    b.formatting.trim_whitespace.with({
-        filetypes = { "tmux", "snippets" },
-    }),
-    b.formatting.stylua.with({
-        condition = with_root_file("stylua.toml"),
-    }),
-    -- diagnostics
-    b.diagnostics.selene.with({
-        condition = with_root_file("selene.toml"),
-    }),
-    b.diagnostics.write_good,
-    b.diagnostics.markdownlint,
-    b.diagnostics.shellcheck.with({
-        diagnostics_format = diagnostics_code_template,
-    }),
-    -- code actions
-    b.code_actions.gitsigns,
-    b.code_actions.gitrebase,
-    -- hover
-    b.hover.dictionary,
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+local formatting = null_ls.builtins.formatting
+-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+local diagnostics = null_ls.builtins.diagnostics
+
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+-- https://github.com/prettier-solidity/prettier-plugin-solidity
+null_ls.setup {
+  debug = false,
+  sources = {
+    formatting.prettier.with {
+      extra_filetypes = { "toml" },
+      extra_args = { "--no-semi", "--single-quote", "--jsx-single-quote" },
+    },
+    formatting.black.with { extra_args = { "--fast" } },
+    formatting.stylua,
+    formatting.google_java_format,
+    diagnostics.flake8,
+  },
+    on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+            vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+            vim.api.nvim_create_autocmd("BufWritePre", {
+                group = augroup,
+                buffer = bufnr,
+                callback = function()
+                    -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                    vim.lsp.buf.formatting_sync()
+                end,
+            })
+        end
+    end,
 }
-
-local M = {}
-M.setup = function(on_attach)
-    if not vim.g.started_by_firenvim then
-        null_ls.setup({
-            -- debug = true,
-            sources = sources,
-            on_attach = on_attach,
-        })
-    end
-end
-
-return M
