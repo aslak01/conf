373a319edead39a05307a20d8e13f0d535e2045f
aslak <aslak.bakkeland@everyday.no>
2022-07-11 23:29:40 +0200

    refactor

--- nvim/lua/user/lsp/null-ls.lua
+++ nvim/lua/user/lsp/null-ls.lua
@@ -24,6 +24,9 @@ null_ls.setup({
 		diagnostics.flake8,
 	},
 	on_attach = function(client, bufnr)
+		if client.name == "tsserver" then
+			client.server_capabilities.document_formatting = false -- 0.7 and earlier
+		end
 		if client.supports_method("textDocument/formatting") then
 			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
 			vim.api.nvim_create_autocmd("BufWritePre", {
